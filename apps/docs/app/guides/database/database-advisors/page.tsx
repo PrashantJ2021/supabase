4688361730ee265825368e5094639c41585b6bee
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-07-08 13:23:56 -0400

    wip add more to the refs page display

--- apps/docs/app/guides/database/database-advisors/page.tsx
+++ apps/docs/app/guides/database/database-advisors/page.tsx
@@ -4,7 +4,8 @@ import { type SerializeOptions } from 'next-mdx-remote/dist/types'
 import rehypeSlug from 'rehype-slug'
 import { Heading } from 'ui'
 import { genGuideMeta } from '~/features/docs/GuidesMdx.utils'
-import { GuideTemplate, MDXRemoteGuides, newEditLink } from '~/features/docs/GuidesMdx.template'
+import { GuideTemplate, newEditLink } from '~/features/docs/GuidesMdx.template'
+import { MDXRemoteBase } from '~/features/docs/MdxBase'
 import { Tabs, TabPanel } from '~/features/ui/Tabs'
 
 import { UrlTransformFunction, linkTransform } from '~/lib/mdx/plugins/rehypeLinkTransform'
@@ -52,7 +53,7 @@ const DatabaseAdvisorDocs = async () => {
 
   return (
     <GuideTemplate meta={meta} editLink={editLink}>
-      <MDXRemoteGuides source={markdownIntro} />
+      <MDXRemoteBase source={markdownIntro} />
       <Heading tag="h2">Available checks</Heading>
       <Tabs listClassNames="flex flex-wrap gap-2 [&>button]:!m-0" queryGroup="lint">
         {lints.map((lint) => (
@@ -62,7 +63,7 @@ const DatabaseAdvisorDocs = async () => {
             label={capitalize(getBasename(lint.path).replace(/_/g, ' '))}
           >
             <section id={getBasename(lint.path)}>
-              <MDXRemoteGuides source={lint.content} options={options} />
+              <MDXRemoteBase source={lint.content} options={options} />
             </section>
           </TabPanel>
         ))}
